129d811da1746dfec76040b085a283382a704e1b
Keng <me@kengblog.com>
2021-09-03 13:29:33 +0900

    add pdfjs test code

---
+++ pdfjs/README.markdown
@@ -0,0 +1,5 @@
+# pdfjs-with-Japanese-test
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/KenginTokyo/pen/XWgKEdP](https://codepen.io/KenginTokyo/pen/XWgKEdP).
+
+
---
+++ pdfjs/dist/script.js
@@ -0,0 +1,23 @@
+var doc = new jsPDF();
+var specialElementHandlers = {
+    '#editor': function (element, renderer) {
+        return true;
+    }
+};
+
+$('#cmd').click(function () {   
+    doc.fromHTML($('#content').html(), 15, 15, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+    // doc.save('sample-file.pdf');
+  doc.output('dataurlnewwindow');
+    // print2newTab(doc)
+});
+
+// function print2newTab(doc){
+//   var string = doc.output('datauristring');
+//   var x = window.open();
+//   x.document.open();
+//   x.document.location=string;
+// }
